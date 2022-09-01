31fcd1093e986652550759459031308daccf116f
ksuzh <ksuzha1@gmail.com>
2022-09-01 21:07:56 +0200

    Added task2

--- seminar1/task2.py
+++ seminar1/task2.py
@@ -5,3 +5,22 @@
 
 # - 1, 4, 8, 7, 5 -> 8
 # - 78, 55, 36, 90, 2 -> 90
+
+numbers = [1, 4, 8, 7, 5]
+max = numbers[0]
+for i in numbers:
+    if i > max:
+        max = i
+print(max)
+
+# numbers = [int(i) for i in input().split()]
+# # numbers = list(map(int, input().split()[:5]))  
+# max = numbers[0]
+# for i in range(len(numbers)):
+#     if numbers[i] > max:
+#         max = numbers[i]
+# print(max)
+
+
+# numbers = [int(i) for i in input().split()[:5]]
+# or numbers = list(map(int,  input().split()[:n]))
